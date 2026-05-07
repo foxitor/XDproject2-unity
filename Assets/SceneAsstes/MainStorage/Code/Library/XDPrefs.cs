fe5698a8233e5f4bb03d124bf481d111c5f7745f
ColdFox_ <brattsev11@gmail.com>
2026-05-07 21:57:42 +0200

    Day3 : menu is almost in place
    Added protected Data Saving
    Added LOD option
    Added Chertalet Wijet (uses tulevo desc for now...)
    Finaly Fixed the EDITOR ui.
    
    Note:
    left to do is - 1.Port all games > 2.Do the advancements for the games > 3. Add more settings.

---
+++ Assets/SceneAsstes/MainStorage/Code/Library/XDPrefs.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+
+namespace XD.Prefs {
+    [System.Serializable]
+    public class AdvancedPPfs {
+        //setup core
+        public static readonly AdvancedPPfs Core = new AdvancedPPfs();
+
+        public static event System.Action OnDataReset;
+
+        ProtectedIntMemory[] BurnIntMemory = new ProtectedIntMemory[0];
+        ProtectedFloatMemory[] BurnFloatMemory = new ProtectedFloatMemory[0];
+
+        public void ProtectedStore(string key, int value) {
+            PlayerPrefs.SetInt(key, value);
+            Protect(key, value);
+            Debug.Log($"Resived Protected-Store Input(int), including : {key} = {value}");
+        }
+        void Protect(string key, int value) {
+            if (FindProtectiveInt(key) != -1) {
+                ProtectedIntMemory burn = BurnIntMemory[FindProtectiveInt(key)];
+                if (burn != null) {
+                    burn.key = key;
+                    burn.value = value;
+                }
+            } else { 
+                var product = new ProtectedIntMemory();
+                product.key = key;
+                product.value = value;
+                //Extend the array that way...
+                ProtectedIntMemory[] temp = new ProtectedIntMemory[BurnIntMemory.Length + 1];
+                System.Array.Copy(BurnIntMemory, temp, BurnIntMemory.Length);
+                temp[temp.Length - 1] = product;
+
+                BurnIntMemory = temp;
+            }
+            foreach(ProtectedIntMemory prot in BurnIntMemory) {
+                //Debug.Log($"{prot.key} = {prot.value}");
+            }
+        }
+        int FindProtectiveInt(string key) {
+            for (int i = 0; i < BurnIntMemory.Length; i++) {
+                if (BurnIntMemory[i].key == key) {
+                    return i;
+                }
+            } 
+            return -1;
+        }
+        int ShareProtectedValue(string key) {
+            for (int i = 0; i < BurnIntMemory.Length; i++) {
+                if (BurnIntMemory[i].key == key) {
+                    return BurnIntMemory[i].value;
+                }
+            } 
+            return -1;
+        }
+        public void Erase() {
+            PlayerPrefs.DeleteAll();
+            Debug.Log($"Attention! Delete protocol is alredy started work,");
+            OnDataReset?.Invoke();
+            Debug.Log($"currently contacting the subscribers.");
+            Debug.Log($"Data must be in place. if some data was completly erased, RIP to theese...");
+        }
+    }
+    [System.Serializable]
+    public class ProtectedIntMemory {
+        public string key;
+        public int value;
+    }
+    [System.Serializable]
+    public class ProtectedFloatMemory {
+        public string key;
+        public float value;
+    }
+}
--- Assets/SceneAsstes/MainStorage/Code/XDButtonBehaviour.cs
+++ Assets/SceneAsstes/MainStorage/Code/XDButtonBehaviour.cs
@@ -1,7 +1,7 @@
 #region Import Libs
 using System.Collections;
 using UnityEngine; using UnityEngine.UI; using UnityEditor; using UnityEngine.SceneManagement;
-using XD.UI;
+using XD.UI; using XD.Prefs;
 #endregion
 #region CustomLibrary
 namespace XD.UI {
@@ -77,14 +77,18 @@ public class XDButtonBehaviour : MonoBehaviour {
                 break;
         }
     }
+    void OnEnable() => AdvancedPPfs.OnDataReset += RestoreDatas;
+    void OnDisable() => AdvancedPPfs.OnDataReset -= RestoreDatas;
     //PlugAdditionss
     void Start() {
         //Button
         if (myButton != null) { myButton.onClick.AddListener(() => {ButtonPressed();}); }
         //Dropdown
-
+        if (myDropdown != null) { myDropdown.onValueChanged.AddListener(delegate {DropdownChanged();}); }
         //Audio
         if (targetMode == UI_target_types.RandomPicker) { myAudio = gameObject.AddComponent<AudioSource>(); }
+
+        RestoreDatas();
     }
     public void ButtonPressed() {
         if (myButton == null) return;
@@ -104,7 +108,11 @@ public class XDButtonBehaviour : MonoBehaviour {
             //Scene manage
             else if (targetMode == UI_target_types.LoadScene) {
                 //'RunScene' or 'ForceQuit'
-                if (ActionName != "ForceQuit") { StartCoroutine(LoadSceneAsync(ActionName)); } else { Application.Quit(); }
+                if (ActionName == "ForceQuit") { 
+                    Application.Quit();
+                } else if (ActionName == "DeletePrefs") {
+                    AdvancedPPfs.Core.Erase();
+                } else { StartCoroutine(LoadSceneAsync(ActionName));  }
             }
         } 
         //ToggleButton
@@ -129,6 +137,31 @@ public class XDButtonBehaviour : MonoBehaviour {
             foreach(GameObject BadObj in SwitcherButtonPiles[switcherState-1].Negative) { BadObj.SetActive(false); }
         }
     }
+    public void DropdownChanged() {
+        if (myDropdown == null) return;
+        //Storing Ints
+        if (targetMode == UI_target_types.StoreIntData) {
+            foreach (string ppf in Strings) {
+                AdvancedPPfs.Core.ProtectedStore(ppf, myDropdown.value);
+                //Debug.Log($"I as a computer programm now indeed store the data, that can be in fact be translated to {ppf}.\nyou may be asking yourself now, whats the value that the {ppf} obtained? so the answer is simple my friend, it is equal to {myDropdown.value}");
+            }
+        }
+    }
+    void RestoreDatas() {
+        //Debug.Log($"RestoreData Was Runned by {gameObject.name}");
+        //Restore Drops
+        if (myDropdown != null) {
+            //RestoreSeasons
+            if (PlayerPrefs.HasKey(Strings[0])) { 
+                myDropdown.value = PlayerPrefs.GetInt(Strings[0]); 
+                //Debug.Log($"RestoreData Was Finished, result : found the ppf");
+                } 
+            else { foreach (string ppf in Strings) { 
+                AdvancedPPfs.Core.ProtectedStore(ppf, myDropdown.value); 
+                } Debug.Log($"RestoreData Was Finished, result : found the protective data");
+            }
+        }
+    }
     IEnumerator LoadSceneAsync(string Name) {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(Name);
         while (!asyncLoad.isDone) {
@@ -139,7 +172,7 @@ public class XDButtonBehaviour : MonoBehaviour {
 #endregion
 #region CustomEditor
 //###   WARNING   ###
-//###     WIP!    ###
+//###    Mess     ###
 //###             ###
 #if UNITY_EDITOR
 [CustomEditor(typeof(XDButtonBehaviour))]
@@ -154,18 +187,18 @@ public class XDButtonBehaviour_Editor : Editor {
         //Strats
         DrawText("-=~=-");
         switch (Elem.type) {
-            case UI_types.button: break;
             case UI_types.switchButton:
                 Elem.switcherVariants = EditorGUILayout.IntField("Max Switcher Variants", Elem.switcherVariants);
                 Elem.switcherDisplayNegatives = EditorGUILayout.Toggle("Display Negatives", Elem.switcherDisplayNegatives);
                 break;
             default:
-                EditorGUILayout.LabelField("No Editor Setting for type");
+                DrawText("No Editor Setting for type");
                 break;
         }
         //Targets
         DrawText("-=~=-");
         switch (Elem.targetMode) {
+            //GameObjs Active
             case UI_target_types.gameObjectsActivity:
                 //SinglePile
                 if (Elem.type == UI_types.button) {
@@ -190,66 +223,39 @@ public class XDButtonBehaviour_Editor : Editor {
                     }
                 }
                 break;
+            //Pulll
             case UI_target_types.RandomPicker :
                 ResizeRandomPullArray(ref Elem.RandomPickerEntries, "  Pull Entries");
                 EditRandomPullArray(Elem.RandomPickerEntries, "    {");
-                for (int i = 0; i < Elem.RandomPickerEntries.Length; i++) {
-                    EditorGUILayout.LabelField("   [#=#]");
-                    //Animators
-                    Elem.RandomPickerEntries[i].TargetAnimator = (Animator)EditorGUILayout.ObjectField(
-                        $"   Element {i}", 
-                        Elem.RandomPickerEntries[i].TargetAnimator, 
-                        typeof(Animator), 
-                        true,
-                        GUILayout.Height(15)
-                    );
-                    //Sounds
-                    Elem.RandomPickerEntries[i].Clip = (AudioClip)EditorGUILayout.ObjectField(
-                        $"   Element {i}", 
-                        Elem.RandomPickerEntries[i].Clip, 
-                        typeof(AudioClip), 
-                        true,
-                        GUILayout.Height(15)
-                    );
-                }
                 break;
+            //Animation Trigger
             case UI_target_types.TriggerAnimation :
                 Elem.SingleAnimator = (Animator)EditorGUILayout.ObjectField(
-                        $"Target Animator", 
-                        Elem.SingleAnimator, 
-                        typeof(Animator), 
-                        true
+                        $"Target Animator", Elem.SingleAnimator, typeof(Animator), true
                     );
                 Elem.ActionName = EditorGUILayout.TextField("Action Name", Elem.ActionName);
                 break;
+            //RunScene
             case UI_target_types.LoadScene :
                 Elem.ActionName = EditorGUILayout.TextField("Scene Name", Elem.ActionName);
                 break;
+            //StoreInt
             case UI_target_types.StoreIntData :
-                int PPfsintPaths = EditorGUILayout.IntField("Paths Length", Elem.Strings.Length, GUILayout.Height(20));
-                if (PPfsintPaths != Elem.Strings.Length) {
-                    System.Array.Resize(ref Elem.Strings, PPfsintPaths);
-                }
-                for (int i = 0; i < Elem.Strings.Length; i++) {
-                    EditorGUILayout.LabelField("   [#=#]");
-                    Elem.Strings[i] = EditorGUILayout.TextField(
-                        $"   Path {i}", 
-                        Elem.Strings[i], 
-                        GUILayout.Height(15)
-                    );
-                }
+                ResizeStringArray(ref Elem.Strings, "   Paths");
+                EditStringArray(Elem.Strings, "   Path");
                 break;
+            //Null
             default :
-            EditorGUILayout.LabelField("No Editor Starts for this Target-type");
+            DrawText("No Editor Starts for this Target-type");
                 break;
         }
-        if (GUI.changed) {
-            EditorUtility.SetDirty(Elem);
-        }
+        if (GUI.changed) { EditorUtility.SetDirty(Elem); }
     }
     void DrawText(string text) {
         EditorGUILayout.LabelField(text);
     }
+
+    //Obj
     void ResizeObjectArray(ref GameObject[] array, string label) {
         int newSize = EditorGUILayout.IntField($"{label} Size", array.Length, GUILayout.Height(20));
         if (newSize != array.Length) System.Array.Resize(ref array, newSize);
@@ -259,12 +265,24 @@ public class XDButtonBehaviour_Editor : Editor {
             array[i] = (GameObject)EditorGUILayout.ObjectField($"{label} Element {i}", array[i], typeof(GameObject), true, GUILayout.Height(15));
         }
     }
+    //Strings
+    void ResizeStringArray(ref string[] array, string label) {
+        int newSize = EditorGUILayout.IntField($"{label} Size", array.Length, GUILayout.Height(20));
+        if (newSize != array.Length) System.Array.Resize(ref array, newSize);
+    }
+    void EditStringArray(string[] array, string label) {
+        for (int i = 0; i < array.Length; i++) {
+            array[i] = EditorGUILayout.TextField($"{label} Element {i}", array[i], GUILayout.Height(15));
+        }
+    }
+    //Pull
     void ResizeRandomPullArray(ref RandomPullEntry[] array, string label) {
         int newSize = EditorGUILayout.IntField($"{label} Size", array.Length, GUILayout.Height(20));
         if (newSize != array.Length) System.Array.Resize(ref array, newSize);
     }
     void EditRandomPullArray(RandomPullEntry[] array, string label) {
         for (int i = 0; i < array.Length; i++) {
+            DrawText("   [#=#]");
             array[i].TargetAnimator = (Animator)EditorGUILayout.ObjectField(
                 $"{label} Element {i}", array[i].TargetAnimator, typeof(Animator), true, GUILayout.Height(15)
             );
